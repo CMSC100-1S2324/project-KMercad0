d7a3a413e9cfd6ef1c296b5644013f35b57c2d36
danjesferrer <dsferrer1@up.edu.ph>
2023-12-01 21:18:52 +0800

    feat: Initialize router with allowed cross origin resource sharing

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,17 @@
 import express from "express";
+import router from "./routes";
 
-// initialize server
+// initialize server.
 const app = express();
 
-// plugin for reading JSON payloads
+// plugin for reading JSON payloads.
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// server listens at Port 3001
+// initialize router.
+router(app);
+
+// server listens at Port 3001.
 app.listen(3001, () => {
     console.log("API listening at port 3001.");
 });
