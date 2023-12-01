9be1f9a9abda8f6777854fe8692aeeb7fd97fbd8
danjesferrer <dsferrer1@up.edu.ph>
2023-12-01 23:29:39 +0800

    test: Added test for addUser

--- backend/test.js
+++ backend/test.js
@@ -1,5 +1,20 @@
 import needle from "needle";
 
-needle.get("http://localhost:3001/users", (err, res) => {
+needle.get("http://localhost:3001/get-users", (err, res) => {
     console.log(res.body);
 });
+
+needle.post(
+    "http://localhost:3001/add-user",
+    {
+        fname: "Jessie",
+        mname: "Jay",
+        lname: "Jose",
+        usertype: "user",
+        email: "jjjose@g.c",
+        password: "jose123",
+    },
+    (err, res) => {
+        console.log(res.body);
+    }
+);
