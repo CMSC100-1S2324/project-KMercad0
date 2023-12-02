ee7d1b7f13d4c998fdcf9e623f7282658a236c2a
danjesferrer <dsferrer1@up.edu.ph>
2023-12-02 15:17:10 +0800

    refactor: Change to Number data type

--- backend/models/product.js
+++ backend/models/product.js
@@ -4,8 +4,8 @@ const ProductSchema = new mongoose.Schema({
     _id: { type: mongoose.Types.ObjectId, required: true },
     title: { type: String, required: true },
     name: { type: String, required: true },
-    type: { type: String, required: true },
-    quantity: { type: String, required: true },
+    type: { type: Number, required: true },
+    quantity: { type: Number, required: true },
 });
 
 const ProductModel = mongoose.model("Product", ProductSchema);
