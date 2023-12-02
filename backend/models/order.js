82458d4730219d7680772104d5a0113a6532d82d
danjesferrer <dsferrer1@up.edu.ph>
2023-12-02 15:19:17 +0800

    feat: Created order model

---
+++ backend/models/order.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const OrderSchema = new mongoose.Schema({
+    _id: { type: mongoose.Types.ObjectId, required: true },
+    productID: { type: String, required: true },
+    quantity: { type: Number, required: true },
+    status: { type: Number, required: true },
+    email: { type: String, required: true },
+    dateOrdered: { type: Date, required: true },
+});
+
+const OrderModel = mongoose.model("Order", OrderSchema);
+export default OrderModel;
