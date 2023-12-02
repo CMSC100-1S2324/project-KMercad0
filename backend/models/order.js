be223f2ecf919f7959b350d7a6c2da2252dab94f
danjesferrer <dsferrer1@up.edu.ph>
2023-12-02 15:35:22 +0800

    refactor: Changed productID data type to ObjectId

--- backend/models/order.js
+++ backend/models/order.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const OrderSchema = new mongoose.Schema({
     _id: { type: mongoose.Types.ObjectId, required: true },
-    productID: { type: String, required: true },
+    productID: { type: mongoose.Types.ObjectId, required: true },
     quantity: { type: Number, required: true },
     status: { type: Number, required: true },
     email: { type: String, required: true },
