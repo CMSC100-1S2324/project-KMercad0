faeb0fe1fb7582a052daf0e2f8e0e5a107dc3813
danjesferrer <dsferrer1@up.edu.ph>
2023-12-02 11:17:50 +0800

    test: Added more test cases

--- backend/test.js
+++ backend/test.js
@@ -1,20 +1,166 @@
 import needle from "needle";
 
+// TEST CASES FOR USERS
 needle.get("http://localhost:3001/get-users", (err, res) => {
-    console.log(res.body);
+    // console.log(res.body);
 });
 
-needle.post(
-    "http://localhost:3001/add-user",
-    {
-        fname: "Jessie",
-        mname: "Jay",
-        lname: "Jose",
-        type: "user",
-        email: "jjjose@g.c",
-        password: "jose123",
-    },
-    (err, res) => {
-        console.log(res.body);
-    }
-);
+// needle.post(
+//     "http://localhost:3001/add-user",
+//     {
+//         fname: "Alan",
+//         mname: "Jay",
+//         lname: "Turing",
+//         type: "user",
+//         email: "ajturing@g.c",
+//         password: "turing123",
+//     },
+//     (err, res) => {
+//         console.log(res.body);
+//     }
+// );
+
+// needle.post(
+//     "http://localhost:3001/add-user",
+//     {
+//         fname: "Dan",
+//         mname: "Jes",
+//         lname: "Ferrer",
+//         type: "user",
+//         email: "djferrer@g.c",
+//         password: "ferrer123",
+//     },
+//     (err, res) => {
+//         console.log(res.body);
+//     }
+// );
+
+// needle.post(
+//     "http://localhost:3001/add-user",
+//     {
+//         fname: "Steph",
+//         mname: "Curry",
+//         lname: "Tanig",
+//         type: "user",
+//         email: "sctanig@g.c",
+//         password: "tanig123",
+//     },
+//     (err, res) => {
+//         console.log(res.body);
+//     }
+// );
+
+// needle.post(
+//     "http://localhost:3001/add-user",
+//     {
+//         fname: "Eric",
+//         mname: "Coy",
+//         lname: "Panga",
+//         type: "user",
+//         email: "ecpanga@g.c",
+//         password: "panga123",
+//     },
+//     (err, res) => {
+//         console.log(res.body);
+//     }
+// );
+
+// needle.post(
+//     "http://localhost:3001/add-user",
+//     {
+//         fname: "Karl",
+//         mname: "Lark",
+//         lname: "Mercado",
+//         type: "user",
+//         email: "klmercado@g.c",
+//         password: "mercado123",
+//     },
+//     (err, res) => {
+//         console.log(res.body);
+//     }
+// );
+
+// needle.post(
+//     "http://localhost:3001/add-user",
+//     {
+//         fname: "Jessie",
+//         mname: "Jay",
+//         lname: "Jose",
+//         type: "user",
+//         email: "jjjose@g.c",
+//         password: "jose123",
+//     },
+//     (err, res) => {
+//         console.log(res.body);
+//     }
+// );
+
+// TEST CASES FOR PRODUCTS
+needle.get("http://localhost:3001/get-products", (err, res) => {
+    // console.log(res.body);
+});
+
+// needle.post(
+//     "http://localhost:3001/add-product",
+//     {
+//         title: "Brown Rice",
+//         name: "Soft and delicious",
+//         type: 1,
+//         quantity: 150,
+//     },
+//     (err, res) => {
+//         console.log(res.body);
+//     }
+// );
+
+// needle.post(
+//     "http://localhost:3001/add-product",
+//     {
+//         title: "Manggos",
+//         name: "Sweet Manggos",
+//         type: 1,
+//         quantity: 100,
+//     },
+//     (err, res) => {
+//         console.log(res.body);
+//     }
+// );
+
+// needle.post(
+//     "http://localhost:3001/add-product",
+//     {
+//         title: "Eggs",
+//         name: "Brown Eggs",
+//         type: 2,
+//         quantity: 500,
+//     },
+//     (err, res) => {
+//         console.log(res.body);
+//     }
+// );
+
+// needle.post(
+//     "http://localhost:3001/add-product",
+//     {
+//         title: "Fresh Chicken",
+//         name: "Submissive and Breedable Chicken",
+//         type: 2,
+//         quantity: 50,
+//     },
+//     (err, res) => {
+//         console.log(res.body);
+//     }
+// );
+
+// needle.post(
+//     "http://localhost:3001/add-product",
+//     {
+//         title: "Fresh Honey",
+//         name: "Honey Honey I love you",
+//         type: 1,
+//         quantity: 200,
+//     },
+//     (err, res) => {
+//         console.log(res.body);
+//     }
+// );
