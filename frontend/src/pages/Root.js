efda6b84e754a79b3f11cd7aee979938a83f9a55
danjesferrer <dsferrer1@up.edu.ph>
2023-12-17 00:34:03 +0800

    feat: Implement different navigations for different user type

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,15 +1,30 @@
 import { Outlet, Link } from "react-router-dom";
 
 export default function Root() {
-    return (
+    const type = localStorage.getItem("type");
+
+    return type === "user" ? (
+        // user
         <>
             <nav>
                 <ul>
                     <li>
-                        <Link to={`/`}>Dashboard</Link>
+                        <Link to={`/dashboard`}>Dashboard</Link>
                     </li>
                     <li>
                         <Link to={`/subjects`}>Subjects</Link>
+                    </li>
+                </ul>
+            </nav>
+            <Outlet />
+        </>
+    ) : (
+        // admin
+        <>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to={`/dashboard`}>Dashboard</Link>
                     </li>
                 </ul>
             </nav>
