3bdd215e1d6145c7fb49c2c64033484502253f2a
danjesferrer <dsferrer1@up.edu.ph>
2023-12-17 23:00:56 +0800

    refactor: Change some fields and add a field for price

--- backend/models/order.js
+++ backend/models/order.js
@@ -4,8 +4,9 @@ const OrderSchema = new mongoose.Schema({
     _id: { type: mongoose.Types.ObjectId, required: true },
     productID: { type: mongoose.Types.ObjectId, ref: "Product", required: true },
     quantity: { type: Number, required: true },
+    price: { type: Number, required: true },
     status: { type: Number, required: true },
-    email: { type: String, ref: "User", required: true },
+    userID: { type: mongoose.Types.ObjectId, ref: "User", required: true },
     dateOrdered: { type: Date, required: true },
 });
 
