8641cfac708fb0c44dbacc82864b650738907a61
danjesferrer <dsferrer1@up.edu.ph>
2023-12-17 15:57:25 +0800

    Revert "feat: Implemented retrieving of user name and type"
    This reverts commit 538efe7edcb233c9be2879b3df5d4a32179cbf34.

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -22,24 +22,9 @@ const retrieveItemsFromCart = async (req, res) => {
     const cart = await User.findOne({ _id: req.body._id }, "cart")
         .populate("cart")
         .then((docs) => docs.cart);
+    console.log(cart);
 
     res.send({ success: true, cart: cart });
 };
 
-const retrieveUserName = async (req, res) => {
-    const username = await User.findOne({ _id: req.body._id }, "fname lname").then(
-        (docs) => `${docs.fname} ${docs.lname}`
-    );
-    console.log(username);
-
-    res.send({ sucess: true, username: username });
-};
-
-const retrieveType = async (req, res) => {
-    const type = await User.findOne({ _id: req.body._id }, "type").then((docs) => docs.type);
-    console.log(type);
-
-    res.send({ sucess: true, type: type });
-};
-
-export { getUsers, addToCart, removeFromCart, retrieveItemsFromCart, retrieveUserName, retrieveType };
+export { getUsers, addToCart, removeFromCart, retrieveItemsFromCart };
