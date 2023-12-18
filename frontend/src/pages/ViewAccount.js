01c33e608ba30740de9d5af1eeacb27ce530093b
danjesferrer <dsferrer1@up.edu.ph>
2023-12-18 13:11:31 +0800

    feat: Implemented viewing all accounts for admin

---
+++ frontend/src/pages/ViewAccount.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import { Card, Container, Row } from "react-bootstrap";
+
+export default function ViewAccount() {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3001/view-all-account")
+            .then((response) => response.json())
+            .then((body) => {
+                setUsers(body);
+            });
+    });
+
+    return (
+        <>
+            <Container>
+                <Row>
+                    {users.map((user, index) => {
+                        return (
+                            <Card key={index} style={{ width: "18rem" }}>
+                                <Card.Body>
+                                    <Card.Title>
+                                        User: {user.fname} {user.mname} {user.lname}{" "}
+                                    </Card.Title>
+                                    <Card.Text>Email: {user.email}</Card.Text>
+                                </Card.Body>
+                            </Card>
+                        );
+                    })}
+                </Row>
+            </Container>
+        </>
+    );
+}
