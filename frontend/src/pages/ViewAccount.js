3f8412228521fab8544ac5a4e67aa34e06d6e26e
danjesferrer <dsferrer1@up.edu.ph>
2024-01-05 19:28:01 +0800

    style: Added design for viewing all accounts

--- frontend/src/pages/ViewAccount.js
+++ frontend/src/pages/ViewAccount.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Card, Container, Row } from "react-bootstrap";
+import { Card, Container, Row, Col } from "react-bootstrap";
 
 export default function ViewAccount() {
     const [users, setUsers] = useState([]);
@@ -12,16 +12,42 @@ export default function ViewAccount() {
             });
     });
 
+    const containerStyle = { fontSize: "24px", paddingTop: "40px", paddingBottom: "40px" };
+    const userCardStyle = {
+        width: "15rem",
+        height: "auto",
+        flexDirection: "column",
+        lineHeight: "1.2",
+        padding: "15px",
+        margin: "10px",
+        border: "0",
+        borderRadius: "10px",
+        backgroundColor: "#d9d9d9",
+    };
+
     return (
-        <>
-            <Container>
-                <div>
-                    Total: <strong>{users.length.toFixed(0)}</strong>
-                </div>
+        <Container fluid>
+            <Container fluid style={containerStyle}>
+                <Row>
+                    <Col>
+                        <h1>
+                            <b>Users</b>
+                        </h1>
+                    </Col>
+                </Row>
+            </Container>
+            <Container fluid className="d-flex align-items-center" style={{ fontSize: "24px", marginBottom: "15px" }}>
+                <Col md="auto">
+                    <div>
+                        Total: <strong>{users.length.toFixed(0)}</strong>
+                    </div>
+                </Col>
+            </Container>
+            <Container fluid>
                 <Row>
                     {users.map((user, index) => {
                         return (
-                            <Card key={index} style={{ width: "18rem" }}>
+                            <Card key={index} style={userCardStyle}>
                                 <Card.Body>
                                     <Card.Title>
                                         User: {user.fname} {user.mname} {user.lname}{" "}
@@ -33,6 +59,6 @@ export default function ViewAccount() {
                     })}
                 </Row>
             </Container>
-        </>
+        </Container>
     );
 }
