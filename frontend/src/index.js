daa62eef20b9f85ce77bcf2f38cbaec4282efb23
danjesferrer <dsferrer1@up.edu.ph>
2024-01-05 14:02:05 +0800

    chore: Removed index.css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, redirect, RouterProvider } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.css";
 
 import Root from "./pages/Root";
 import Auth from "./pages/Auth";
